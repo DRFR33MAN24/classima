fac22985142f431ff413c5ca600d78b3b395eabb
Gordon Freeman <drfr33man@protonmail.com>
2022-11-14 02:41:24 +0200

    worked on style

--- app/services/socialLoginConfig.js
+++ app/services/socialLoginConfig.js
@@ -1,13 +1,13 @@
 export const socialConfig = {
-  enabled: false,
-  socialPlatforms: [], // Currently we only support Facebook and Google signin. We'll add more in future
+  enabled: true,
+  socialPlatforms: ["facebook","google"], // Currently we only support Facebook and Google signin. We'll add more in future
   google: {
     webClientId:
       "745814608905-8o6u8nuvqtvl46bj71fplh1r51ev48um.apps.googleusercontent.com",
   },
   facebook: {
-    appID: "87867657656546545",
-    appName: "Your Facebook App Name",
+    appID: "2308084912684858",
+    appName: "sailstore",
     android: {},
     iOS: {},
   },
